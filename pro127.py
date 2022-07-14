8ad0459fc4f606fc92cc074937579b3c15a9a070
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-14 15:29:58 +0530

    Add files via upload

---
+++ pro127.py
@@ -0,0 +1,34 @@
+import csv
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+start_url="https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser=webdriver.Chrome("C:\WHITEHAT 26-7-21\Python\chromedriver.exe")
+
+browser.get(start_url)
+time.sleep(15)
+
+def scrap():
+    headers=["Name","Distance","Mass","Radius"]
+    star_data=[]
+    for i in range(0,102):
+        soup=BeautifulSoup(browser.page_source,"html.parser")
+        for th_tag in soup.find_all("th",attrs={"class","exsoplanet"}):
+            tr_tags=th_tag.find_all("tr")
+            templist=[]
+            for index,li_tag in enumerate(tr_tags):
+                if index==0:
+                    templist.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        templist.append(li_tag.contents[0])
+                    except:
+                        templist.append("")
+            star_data.append(templist)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+    with open("Planets.csv","w") as f:
+        csvwriter=csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(star_data)
+scrap()
